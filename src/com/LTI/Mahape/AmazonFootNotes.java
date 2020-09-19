d53d9d441239e20c0169f7be127c5ba348f97535
juhi <juhisehgal31@gmail.com>
2020-09-19 10:29:59 +0530

    added a new comment on 19092020

--- src/com/LTI/Mahape/AmazonFootNotes.java
+++ src/com/LTI/Mahape/AmazonFootNotes.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//hello today is monday
+//hello today is monday and it is a sunny day
 public class AmazonFootNotes {
 
 	public static void main(String[] args) throws Exception{
